30615fe4c5db34165af40c9e643eede8e3cb2530
Ashley <ashley.honn@gmail.com>
2021-05-26 22:40:37 -0700

    commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to IS657 Ashley Honn</Text>
+      <Text>Welcome to IS657 Ashley Honn! </Text>
       <StatusBar style="auto" />
     </View>
   );
